afe78715ae2d1e6879666cbe4a97b5d2f9d6a253
raon1961 <luou0819@gmail.com>
2025-08-22 09:07:02 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 const members = [
   { id: 1, name: "HAPPY", profileImg: "images/leo1-profile.jpg", detailImg: "images/leo1-detail.jpg", bio: "리오야 포기하지 않아줘서 고마워." },
-  { id: 2, name: "BIRTHDAY", profileImg: "images/leo2-profile.jpg", detailImg: "images/leo2-detail.jpg", bio: "늘 너 자신을 믿고 달리자. 그럼 무엇이든 할 수 있을거야." },
+  { id: 2, name: "BIRTHDAY", profileImg: "images/leo2-profile.jpg", detailImg: "images/leo2-detail.jpg", bio: "늘 너 자신을 믿고 달리자 그럼 무엇이든 할 수 있을거야." },
   { id: 3, name: "LEE LEO", profileImg: "images/leo3-profile.jpg", detailImg: "images/leo3-detail.jpg", bio: "행복한 생일이 되길." },
   { id: 4, name: "LEO FANS", profileImg: "images/leo4-profile.jpg", detailImg: "images/leo4-detail.jpg", bio: "리오 맛있는거 많이 먹어." },
   { id: 5, name: "WANT", profileImg: "images/leo5-profile.jpg", detailImg: "images/leo5-detail.jpg", bio: "데뷔하겠습니다!!" },
-  { id: 6, name: "LEO'S DEBUT", profileImg: "images/leo6-profile.jpg", detailImg: "images/leo6-detail.jpg", bio: "리오 사랑해!" },
-  { id: 7, name: "리오야 생일 축하해!", profileImg: "images/leo7-profile.jpg", detailImg: "images/leo7-detail.jpg", bio: "리오 사랑해!" },
-  { id: 8, name: "리오 꼭 데뷔하자!", profileImg: "images/leo8-profile.jpg", detailImg: "images/leo8-detail.jpg", bio: "리오 사랑해!" },
+  { id: 6, name: "LEO'S DEBUT", profileImg: "images/leo6-profile.jpg", detailImg: "images/leo6-detail.jpg", bio: "리오 약속 지키자 꼭 데뷔해!" },
+  { id: 7, name: "리오야 생일 축하해!", profileImg: "images/leo7-profile.jpg", detailImg: "images/leo7-detail.jpg", bio: "리오야 너는 잘하고 있어 우린 너 믿는다." },
+  { id: 8, name: "리오 꼭 데뷔하자!", profileImg: "images/leo8-profile.jpg", detailImg: "images/leo8-detail.jpg", bio: "항 상 빛 나 리 오" },
   { id: 9, name: "리오 잘 하고 있어!", profileImg: "images/leo9-profile.jpg", detailImg: "images/leo9-detail.jpg", bio: "리오야 나도 리오 꼭 데뷔하면 좋겠어" }
 ];
 
